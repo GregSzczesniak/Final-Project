0e7e1f4e2ff6aa26b13cbf5af11ad39776b01fe5
GregSzczesniak <grze.szczesniak@gmail.com>
2016-09-01 21:41:17 +0200

    v

--- Wikto/gulpfile.js
+++ Wikto/gulpfile.js
@@ -4,13 +4,13 @@ var sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("scss", function() {
     return gulp.src("sass/main.scss")
-        .pipe(sourcemaps.init())
+        //.pipe(sourcemaps.init())
         .pipe(sass ({
             errLogConsole: true,
             outputStyle: "compressed",
             //sourceComments: true,
         }).on("error", sass.logError))
-.pipe(sourcemaps.write())
+//.pipe(sourcemaps.write())
 .pipe(gulp.dest("css"))
 })
 
--- Wikto/js/app.js
+++ Wikto/js/app.js
@@ -9,15 +9,119 @@ $(function(){
 		rotateR = $('#btn-transform-right'),
 		rotateL = $('#btn-transform-left'),
 		body = $('body'),
-		homeGrid = $('home-grid');
+		homeGrid = $('home-grid'),
+		brush = $('.brush'),
+		pencil = $('.pencil'),
+		laptop = $('.laptop'),
+		graphics = $('#graphics'),
+		paintings = $('#paintings'),
+		projects = $('#projects'),
+		gallery = $('.gallery-content'),
+		icons = $('.icon'),
+		menuIcons = $('.menu-icon'),
+		links = $('.menu-link'),
+		paintLink = $('.paintings-link'),
+		graphLink = $('.graphics-link'),
+		projectLink = $('.projects-link'),
+		home = $('.home'),
+		diamond = $('.diamond'),
+		diamondBig = $('.diamond-big');
 
-	
+
+	// home.on('click', function(event){
+	// 	event.preventDefault();
+	// 	var href = $(this).attr('href');
+
+	// 	$('body').delay(200).animate({
+	// 		scrollTop: $(href).offset().top
+	// 	}, 1000);
+
+	// });
+
+
+
+	var cal = 1.9;
+
+	$(window).resize(function(){
+		var sizeD = diamond.width(),
+			sizeI = menuIcons.height(),
+			newSizeI = sizeD / cal;
+
+		// var newSizeI = 
+		console.log("wymiar rombu ", sizeD);
+		console.log("wymiar ikony ", sizeI);
+		console.log("stosunek ", cal);
+		console.log("nowy wymiar ikony ", newSizeI);
+
+
+		menuIcons.css('font-size', newSizeI);
+	})
+
+	paintLink.on('click', function(event){
+		event.preventDefault();
+		var href = $(this).attr('href'),
+			id = $(this).data('id');
+
+		if ($(href).is(':hidden')) {
+
+			$('body').delay(200).animate({
+				scrollTop: $(id).offset().top
+			}, 1000);
+			showGallery(brush, paintings);
+		} else {
+
+			$('body').delay(200).animate({
+				scrollTop: $(href).offset().top
+			}, 1000);
+		}
+	});
+
+	graphLink.on('click', function(event){
+		event.preventDefault();
+		var href = $(this).attr('href'),
+			id = $(this).data('id');
+
+		if ($(href).is(':hidden')) {
+			
+			$('body').delay(200).animate({
+				scrollTop: $(id).offset().top
+			}, 1000);
+			showGallery(pencil, graphics);
+		} else {
+
+			$('body').delay(200).animate({
+				scrollTop: $(href).offset().top
+			}, 1000);
+			
+		}
+	});
+
+	projectLink.on('click', function(event){
+		event.preventDefault();
+		var href = $(this).attr('href'),
+			id = $(this).data('id');
+
+		if ($(href).is(':hidden')) {
+			
+			$('body').delay(200).animate({
+				scrollTop: $(id).offset().top
+			}, 1000);
+			showGallery(laptop, projects);
+		} else {
+
+			$('body').delay(200).animate({
+				scrollTop: $(href).offset().top
+			}, 1000);
+			
+		}
+	});
+
+	body.addClass('overflowX');
 
 	item.on('click', function(){
 		var $this = $(this),
 			$that = $this.siblings('menu-item');
 
-		// body.css('overflowX', 'hidden');
 		item.removeClass('menu-item-current');
 		$this.addClass('menu-item-current');
 	})
@@ -38,30 +142,51 @@ $(function(){
 		$(this).toggleClass('grow');
 	})
 
-	rotateR.on('click', function(){
-		currentPic.removeClass('rotateL rotateGrowL');
-		if (currentPic.hasClass('grow')) {
-			currentPic.toggleClass('rotateGrowR');
-		} else {
-			currentPic.toggleClass('rotateR');
-		}
-		
-	})
-
-	rotateL.on('click', function(){
-		currentPic.removeClass('rotateR rotateGrowR');
-		if (currentPic.hasClass('grow')) {
-			currentPic.toggleClass('rotateGrowL');
-		} else {
-			currentPic.toggleClass('rotateL');
-		}
-	})
-
 	btn.on('click', function(){
 		itemView.removeClass('preview-open preview-image-loaded');
-		currentPic.attr('src', '').removeClass('animate grow rotateL rotateR rotateGrowL rotateGrowR');
+		currentPic.attr('src', '').removeClass('animate grow');
 		body.removeClass('overflow');
 	})
+
+	function showGallery(icon, exposition) {
+		icons.css('color', 'rgba(0, 0, 0, .6)');
+		icon.css('color','rgba(0, 0, 0, 1)');
+		gallery.addClass('hide');
+		exposition.removeClass('hide').addClass('fade');
+	}
+
+	pencil.on('click', function(){
+		showGallery(pencil, graphics);
+	})
+
+	brush.on('click', function(){
+		showGallery(brush, paintings);
+	})
+
+	laptop.on('click', function(){
+		showGallery(laptop, projects);
+	})
+
+	// pencil.on('click', function(){
+	// 	icons.css('color', 'rgba(0, 0, 0, .6)');
+	// 	pencil.css('color','rgba(0, 0, 0, 1)');
+	// 	gallery.addClass('hide');
+	// 	graphics.removeClass('hide').addClass('fade');
+	// })
+
+	// brush.on('click', function(){
+	// 	icons.css('color', 'rgba(0, 0, 0, .6)');
+	// 	brush.css('color','rgba(0, 0, 0, 1)');
+	// 	gallery.addClass('hide');
+	// 	paintings.removeClass('hide').addClass('fade');
+	// })
+
+	// laptop.on('click', function(){
+	// 	icons.css('color', 'rgba(0, 0, 0, .6)');
+	// 	laptop.css('color','rgba(0, 0, 0, 1)');
+	// 	gallery.addClass('hide');
+	// 	projects.removeClass('hide').addClass('fade');
+	// })
 
 })
 
